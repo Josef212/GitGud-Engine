9e199abea4daf379a6097b2f920b7ac08108c8b0
Josef21296 <jcasanovasv@hotmail.com>
2017-04-17 20:30:57 +0200

    Resource texture and material, still todo

--- GitGud/GitGud/ResourceMaterial.h
+++ GitGud/GitGud/ResourceMaterial.h
@@ -1,8 +1,46 @@
 #ifndef __RESOURCE_MATERIAL_H__
 #define __RESOURCE_MATERIAL_H__
 
-
 #include "Resource.h"
+#include "ResourceTexture.h"
+#include "Color.h"
+#include <string>
+#include <vector>
+#include <map>
+
+enum SHADING_MODE
+{
+	SHADING_FLAT = 0x1,
+	SHADING_GOURAUD = 0x2,
+	SHADING_PHONG = 0x3,
+	SHADING_BLINN = 0x4,
+	SHADING_TOON = 0x5,
+	SHADING_ORENAYAR = 0x6,
+	SHADING_MINNAERT = 0x7,
+	SHADING_COOLTORRANCE = 0x8,
+	SHADING_NO_SHADING = 0x9,
+	SHADING_FRESNEL = 0xA
+};
+
+enum BLEND_MODE
+{
+	/**
+	*sourceColor * sourceAlpha + destColor * (1 - sourceAlpha)
+	*/
+	BLEND_DEFAULT = 0x0,
+
+	/**
+	*sourceColor * 1 + destColor * 1
+	*/
+	BLEND_ADDITIVE = 0x1
+};
+
+enum RENDER_MODE
+{
+	RENDER_OPAQUE = 0x0,
+	RENDER_CUTOUT = 0x1,
+	RENDER_TRANSPARENT = 0x2
+};
 
 class ResourceMaterial : public Resource
 {
@@ -12,6 +50,48 @@ public:
 
 	bool LoadInMemory()override;
 	bool RemoveFromMemory()override;
+
+public:
+	//TODO: Must find a way to organiza propertirs. For now will hardcode a generic material based on unity standard material shader
+	
+	UID shader = 0;
+
+	enum RENDER_MODE renderMode = RENDER_OPAQUE;
+	
+	UID ambientTexture = 0;
+	Color ambientColor = White;
+
+	UID diffuseTexture = 0;
+	Color diffuseColor = White;
+
+	//-----------
+
+	UID metallicTexture = 0;
+	Color metallicColor = White;
+	float metallicValue = 0.0f; //0-1
+	float metallicSmothness = 0.5f;//0-1
+	//TODO: Source: metallic alpha / albedo alpha
+
+	UID specularTexture = 0;
+	Color specularColor = White;
+	float specularSmothness = 0.5f;//0-1
+	//TODO: Source: metallic alpha / albedo alpha
+
+	//------------
+
+	UID normalMap = 0;
+	UID heightMap = 0;
+	UID occlusion = 0;
+	UID emissionTexture = 0;
+	Color emissionColor = Black;
+	float emissionValue = 0;
+	//Global ilumin: none/realtime/baked
+
+	UID detailMask = 0;
+	 //TODO: Tiling/offset
+	//TODO: Secondaty maps
+	
+
 };
 
 #endif // !__RESOURCE_MATERIAL_H__
--- GitGud/GitGud/ResourceTexture.h
+++ GitGud/GitGud/ResourceTexture.h
@@ -1,8 +1,38 @@
 #ifndef __RESOURCE_TEXTURE_H__
 #define __RESOURCE_TEXTURE_H__
 
-
 #include "Resource.h"
+
+enum TEXTURE_TYPE
+{
+	TEX_NONE = 0x0,
+	TEX_DIFFUSE = 0x1,
+	TEX_SPECULAR = 0x2,
+	TEX_AMBIENT = 0x3,
+	TEX_EMISSIVE = 0x4,
+	TEX_HEIGHT = 0x5,
+	TEX_NORMAL_MAP = 0x6,
+	TEX_SHININESS = 0x7,
+	TEX_OPACITY = 0x8,
+	TEX_DISPLACEMENT = 0x9,
+	TEX_LIGHT_MAP = 0xA,
+	TEX_REFLECTION = 0xB
+};
+
+enum TEXTURE_MAP_MODE
+{
+	TEX_WRAP = 0x1,
+	TEX_CLAMP = 0x2,
+	TEX_DECAL = 0x3,
+	TEX_MIRROR = 0x4
+};
+
+enum TEXTURE_FLAG
+{
+	TEX_FLAG_INVERT = 0x1,
+	TEX_FLAG_USE_ALPHA = 0x2,
+	TEX_FLAG_IGNORE_ALPHA = 0x4
+};
 
 class ResourceTexture : public Resource
 {
