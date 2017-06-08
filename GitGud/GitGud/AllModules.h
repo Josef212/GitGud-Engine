097e5e87d1f885addf5d52a3adee7c520563d73b
Josef21296 <jcasanovasv@hotmail.com>
2017-06-08 23:54:34 +0200

    Modules sum up

---
+++ GitGud/GitGud/AllModules.h
@@ -0,0 +1,14 @@
+#ifndef __ALLMODULES_H__
+#define __ALLMODULES_H__
+
+#include "Module.h"
+#include "M_Camera3D.h"
+#include "M_FileSystem.h"
+#include "M_GoManager.h"
+#include "M_Input.h"
+#include "M_Renderer.h"
+#include "M_Window.h"
+#include "M_ResourceManager.h"
+#include "M_Editor.h"
+
+#endif // !__ALLMODULES_H__
