244a2d71328e394e5ff976dae68a5c42f1dbdab4
Josef21296 <jcasanovasv@hotmail.com>
2017-06-09 15:13:03 +0200

    Changed cube prefab

--- GitGud/GitGud/ImporterMesh.cpp
+++ GitGud/GitGud/ImporterMesh.cpp
@@ -318,29 +318,12 @@ bool ImporterMesh::LoadCube(ResourceMesh * res)
 	//-----------------------
 
 	uint indices[6 * 6] = {
-		//Bottom
-		3, 1, 0,	
-		3, 2, 1,
-
-		//Left
-		3 + 4 * 1, 1 + 4 * 1, 0 + 4 * 1,
-		3 + 4 * 1, 2 + 4 * 1, 1 + 4 * 1,
-
-		// Front
-		3 + 4 * 2, 1 + 4 * 2, 0 + 4 * 2,
-		3 + 4 * 2, 2 + 4 * 2, 1 + 4 * 2,
-
-		// Back
-		3 + 4 * 3, 1 + 4 * 3, 0 + 4 * 3,
-		3 + 4 * 3, 2 + 4 * 3, 1 + 4 * 3,
-
-		// Right
-		3 + 4 * 4, 1 + 4 * 4, 0 + 4 * 4,
-		3 + 4 * 4, 2 + 4 * 4, 1 + 4 * 4,
-
-		// Top
-		3 + 4 * 5, 1 + 4 * 5, 0 + 4 * 5,
-		3 + 4 * 5, 2 + 4 * 5, 1 + 4 * 5
+		0,  1,  2,  0,  2,  3,   //front
+		4,  5,  6,  4,  6,  7,   //right
+		8,  9,  10, 8,  10, 11,  //back
+		12, 13, 14, 12, 14, 15,  //left
+		16, 17, 18, 16, 18, 19,  //upper
+		20, 21, 22, 20, 22, 23	 //bottom
 	};
 
 	res->numIndices = 6 * 6;
@@ -351,18 +334,18 @@ bool ImporterMesh::LoadCube(ResourceMesh * res)
 	float s = 0.5f;
 
 	float vertices[12 * 6] = {
-		//Bottom
-		-s, -s, s,   s, -s, s,   s, -s, -s,   -s, -s, -s,
-		//Left
-		-s, s, -s,   -s, s, s,   -s, -s, s,   -s, -s, -s,
 		//Front
-		-s, s, s,   s, s, s,   s, -s, s,   -s, -s, s,
-		//Back
-		s, s, -s,   -s, s, -s,   -s, -s, -s,   s, -s, -s,
+		-s, -s, s,	s, -s, s,	s, s, s,	-s, s, s,
 		//Right
-		s, s, s,   s, s, -s,   s, -s,   -s, s, -s, s,
+		s, s, s,	s, s, -s,	s, -s, -s,	s, -s, s,
+		//Back
+		-s, -s, -s,		s, -s, -s,	s, s, -s,	-s, s, -s,
+		//Left
+		-s, -s, -s,		-s, -s, s,	-s, s, s,	-s, s, -s,
 		//Top
-		-s, s, -s,   s, s, -s,   s, s, s,   -s, s, s
+		s, s, s,	-s, s, s,	-s, s, -s,	 s, s, -s,
+		//Bot
+		-s, -s, -s,		s, -s, -s,	 s, -s, s,	 -s, -s, s
 	};
 
 	res->numVertices = 24;
@@ -373,38 +356,38 @@ bool ImporterMesh::LoadCube(ResourceMesh * res)
 	float u = 1.0f;
 
 	float normals[12 * 6] = {
-		//Bottom
-		0.0f, -u, 0.0f,		0.0f, -u, 0.0f,		0.0f, -u, 0.0f,		0.0f, -u, 0.0f,
-		//Left
-		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,
 		//Front
 		0.0f, 0.0f, u,		0.0f, 0.0f, u,		0.0f, 0.0f, u,		0.0f, 0.0f, u,
-		//Back
-		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,
 		//Right
 		u, 0.0f, 0.0f,		u, 0.0f, 0.0f,		u, 0.0f, 0.0f,		u, 0.0f, 0.0f,
+		//Back
+		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,		0.0f, 0.0f, -u,
+		//Left
+		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,		-u, 0.0f, 0.0f,
 		//Top
 		0.0f, u, 0.0f,		0.0f, u, 0.0f,		0.0f, u, 0.0f,		0.0f, u, 0.0f,
+		//Bot
+		0.0f, -u, 0.0f,		0.0f, -u, 0.0f,		0.0f, -u, 0.0f,		0.0f, -u, 0.0f
 	};
 
 	res->normals = new float[res->numVertices * 3];
 	memcpy(res->normals, normals, sizeof(float) * res->numVertices * 3);
 
 	//-----------------------
-
+	float o = 0.0f;
 	float uvs[8 * 6] = {
-		//Bottom
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
-		//Left
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
 		//Front
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
-		//Back
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
+		o, o,		u, o,	u, u,	o, u,
 		//Right
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
+		o, u,		u, u,	u, o,	o, o,
+		//Back
+		u, o,		o, o,	o, u,	u, u,
+		//Left
+		o, o,		u, o,	u, u,	o, u,
 		//Top
-		u, u,	0.f, u,		0.0f, 0.0f,		u, 0.0f,
+		u, o,		o, o,	o, u,	u, u,
+		//Bot
+		u, u,		o, u,	o, o,	u, o
 	};
 
 	res->uvs = new float[res->numVertices * 2];
@@ -413,18 +396,18 @@ bool ImporterMesh::LoadCube(ResourceMesh * res)
 	//-----------------------
 
 	float colors[12 * 6] = {
-		//Bottom
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,
-		//Left
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,
-		//Front
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,
-		//Back
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,
-		//Right
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f,
-		//Top
-		1.0f, 0.0f, 0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f, 1.0f
+		//Front		0
+		1.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 0.0f, 0.0f,
+		//Right		6
+		0.0f, 1.0f, 0.0f,	0.0f, 1.0f, 0.0f,	0.0f, 1.0f, 0.0f,	0.0f, 1.0f, 0.0f,
+		//Back		12
+		0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f, 1.0f,
+		//Left		18
+		1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f,	1.0f, 1.0f, 0.0f,
+		//Top		24
+		0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f,	0.0f, 1.0f, 1.0f,
+		//Bot		30
+		1.0f, 0.0f, 1.0f,	1.0f, 0.0f, 1.0f,	1.0f, 0.0f, 1.0f,	1.0f, 0.0f, 1.0f
 	};
 
 	res->colors = new float[res->numVertices * 3];
