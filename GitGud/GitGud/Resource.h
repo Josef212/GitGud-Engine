ae4161ed655d36f37b1ff4e7586a3bae0b1e3975
Josef21296 <jcasanovasv@hotmail.com>
2017-09-19 23:15:03 +0200

    Code comment on resource class

--- GitGud/GitGud/Resource.h
+++ GitGud/GitGud/Resource.h
@@ -22,53 +22,66 @@ public:
 	{}
 
 	virtual ~Resource()
-	{}
+	{
+		FreeResource();
+	}
 
+	/** GetType: Return the resource type. */
 	RESOURCE_TYPE GetType()const
 	{
 		return type;
 	}
 
+	/** GetUID: Return the resource unique identifier. */
 	UID GetUID()const
 	{
 		return uuid;
 	}
 
+	/**Count: Return the ammount of instances used/loaded. */
 	uint Count()const
 	{
 		return instancesLoaded;
 	}
 
+	/** IsInMemory: Return true if is at least one instance in memory. */
 	bool IsInMemory()const
 	{
 		return instancesLoaded > 0;
 	}
 
+	/** GetOriginalFile: Return the original file of the resource. */
 	const char* GetOriginalFile()const
 	{
 		return originalFile.GetFile();
 	}
 
+	/** GetOriginalFileFullPath: Return the resource original file full path. */
 	const char* GetOriginalFileFullPath()const
 	{
 		return originalFile.GetFullPath();
 	}
 
+	/** GetExportedFile: Return the resource exported file. */
 	const char* GetExportedFile()const
 	{
 		return exportedFile.GetFile();
 	}
 
+	/** GetExportedFileFullPath: Return the resource exported full path. */
 	const char* GetExportedFileFullPath()const
 	{
 		return exportedFile.GetFullPath();
 	}
 
+	/**GetResourceName: Return the resource name. */
 	const char* GetResourceName()const
 	{
 		return name.c_str();
 	}
 
+	/** LoadToMemory: If the resource is already in memory just adds an instance to counter otherwise calls the overloaded LoadInMemory method to current load it for first time. 
+						Return true if could load it properly, false if not. */
 	bool LoadToMemory()
 	{
 		if (instancesLoaded > 0)
@@ -79,6 +92,8 @@ public:
 		return instancesLoaded > 0;
 	}
 
+	/** FreeResource: If there are several instances in memory just remove one from the counter, otherwise if is the last isntace (counter == 1) calls the overloaded RemoveFromMemory method to actually free the resource.
+						Return true if the resource was currently freed, false if could not free it or was already freed. */
 	bool FreeResource()
 	{
 		bool ret = false;
@@ -91,18 +106,24 @@ public:
 		return ret;
 	}
 
+	/** AddInstance: Just add one to the instance counter. */
 	void AddInstance()
 	{
 		++instancesLoaded;
 	}
 
+	/** RemoveInstace: Just remove one instace from the instances counter if is higher than one. If there was only one left also free the resource from memory. */
 	void RemoveInstance()
 	{
-		--instancesLoaded;
-		if (instancesLoaded <= 0)
-			RemoveFromMemory();
+		if (instancesLoaded > 0)
+		{
+			if (instancesLoaded - 1 == 0)
+				RemoveFromMemory();
+			--instancesLoaded;
+		}
 	}
 
+	/** Save: Saves into the json object the resource UID, type, original file, exported file and name. Also calls the overloaded OnSave method. */
 	void Save(JsonFile& file)
 	{
 		file.AddInt("UID", uuid);
@@ -113,6 +134,7 @@ public:
 		OnSave(file);
 	}
 
+	/** Load: Load into the resource from the json object the resource UID, type, original file, exported file and name. Also calls the overloaded OnLoad method. */
 	void Load(JsonFile& file)
 	{
 		uuid = file.GetInt("UID", 0);
@@ -123,9 +145,13 @@ public:
 	}
 
 protected:
+	/** OnSave: Pure virtual method called while saving the resource. */
 	virtual void OnSave(JsonFile& file) {}
+	/** OnLoad: Pure virtual method called while laoding the resource. */
 	virtual void OnLoad(JsonFile& file) {}
+	/** LoadInMemory: Pure virtual method called when want to actually load the resource into memory. For example a mesh or a texture. */
 	virtual bool LoadInMemory() = 0;
+	/** RemoveFromMemory: Pure virtual method called when want to actually free the resource from memory. For example a mesh or a texture. */
 	virtual bool RemoveFromMemory() = 0;
 
 public:
