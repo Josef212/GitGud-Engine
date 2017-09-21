c0d3637bc86a76a17d850cf47ac6b93d2f424082
Josef21296 <jcasanovasv@hotmail.com>
2017-09-21 13:19:40 +0200

    Log prints time and date

--- GitGud/GitGud/Globals.cpp
+++ GitGud/GitGud/Globals.cpp
@@ -2,24 +2,29 @@
 #include <SDL.h>
 #include "App.h"
 #include <assert.h>
+#include "MathGeoLib\include\Time\Clock.h"
 
 void _log(LOG_TYPE type, const char file[], int line, const char* format, ...)
 {
 	static char tmpString[4096];
 	static char tmpString2[4096];
+	static char timeStr[1024];
 	static va_list ap;
 
 	va_start(ap, format);
 	vsprintf_s(tmpString, 4096, format, ap);
 	va_end(ap);
-	sprintf_s(tmpString2, 4096, "\n%s::%s(%d): %s", GetLogTypeStr(type), file, line, tmpString);
+
+	sprintf_s(timeStr, 1024, "(%d-%d-%d %d:%d:%d)", Clock::Day(), Clock::Month(), Clock::Year(), Clock::Hour(), Clock::Min(), Clock::Sec());
+
+	sprintf_s(tmpString2, 4096, "\n%s %s::%s(%d): %s", timeStr, GetLogTypeStr(type), file, line, tmpString);
 
 	//OutputDebugString(tmpString2);
 	SDL_Log(tmpString2);
 
 	if (app)
 	{
-		sprintf_s(tmpString2, 4096, "%s::%s\n", GetLogTypeStr(type), tmpString);
+		sprintf_s(tmpString2, 4096, "%s %s::%s\n", timeStr, GetLogTypeStr(type), tmpString);
 		app->Log(tmpString2, type);
 	}
 }
